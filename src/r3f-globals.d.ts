f450a587d52d0e07c5f9b5607964701d1746ead3
Banderos14 <anton.shyshenko@gmail.com>
2026-05-20 10:01:23 +0200

    Fix GitHub Pages deployment

--- src/r3f-globals.d.ts
+++ src/r3f-globals.d.ts
@@ -1,4 +1,18 @@
 // Importing this module triggers @react-three/fiber's global JSX augmentation
 // (declare global { namespace JSX { interface IntrinsicElements extends ThreeElements } })
 import '@react-three/fiber';
+import type { ThreeElements } from '@react-three/fiber';
+
+declare module 'react' {
+  namespace JSX {
+    interface IntrinsicElements extends ThreeElements {}
+  }
+}
+
+declare module 'react/jsx-runtime' {
+  namespace JSX {
+    interface IntrinsicElements extends ThreeElements {}
+  }
+}
+
 export {};
